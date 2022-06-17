9473fa833478e1723b7a349883390c7359553bc6
eurico77 <euricomagalhaes61@gmail.com>
2022-06-17 12:43:28 -0300

    feat: ensure signup controller return 400 if no email provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,13 +5,27 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        email: '',
-        password: '',
-        passwordConfirmation: '',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
       },
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param Name'));
+  });
+
+  it('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param Email'));
   });
 });
