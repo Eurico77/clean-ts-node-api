2fd4afcd2fc8bd8db7c667d5a95e45946f00710d
eurico77 <euricomagalhaes61@gmail.com>
2022-07-04 20:13:52 -0300

    feat: ensure validation email for an email invalid is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,21 +2,26 @@ import {
   EmailValidator, Controller, HttpRequest, HttpResponse,
 } from '../contracts';
 import { MissingParamError, InvalidParamError } from '../errors';
-import { badRequest } from '../helpers/http-helper';
+import { badRequest, serverError } from '../helpers/http-helper';
 
 export class SignUpController implements Controller {
   constructor(private readonly emailValidation: EmailValidator) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+        const isValid = this.emailValidation.isValid(httpRequest.body.email);
+        if (!isValid) {
+          return badRequest(new InvalidParamError('email'));
+        }
       }
-      const isValid = this.emailValidation.isValid(httpRequest.body.email);
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'));
-      }
+    } catch (error) {
+      console.error(error);
+      return serverError();
     }
   }
 }
