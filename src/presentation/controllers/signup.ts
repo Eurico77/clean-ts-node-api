55ee2e2157b7da9f7ed6204b101eb24d972216a8
eurico77 <euricomagalhaes61@gmail.com>
2022-07-04 08:30:07 -0300

    refactor: refactor imports

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,21 @@
-import { Controller } from '../contracts/controller';
-import { HttpRequest, HttpResponse } from '../contracts/http';
-import { MissingParamError } from '../errors/missing-param-error';
+import {
+  EmailValidator, Controller, HttpRequest, HttpResponse,
+} from '../contracts';
+import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController implements Controller {
+  constructor(private readonly emailValidation: EmailValidator) {}
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
+      }
+      const isValid = this.emailValidation.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
       }
     }
   }
