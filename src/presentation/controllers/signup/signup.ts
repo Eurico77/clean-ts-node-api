db4fb10064775473ee73c84d4a85182ec7e1af98
eurico77 <euricomagalhaes61@gmail.com>
2022-07-05 08:11:09 -0300

    refactor: adjustments in import modules in context

---
+++ src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,53 @@
+import { MissingParamError, InvalidParamError } from '../../errors';
+import { badRequest, serverError } from '../../helpers/http-helper';
+import {
+  EmailValidator,
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  AddAccount,
+} from './signup-protocols';
+
+export class SignUpController implements Controller {
+  constructor(
+    private readonly emailValidation: EmailValidator,
+    private readonly addAccount: AddAccount,
+  ) {}
+
+  handle(httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+      const {
+        name, email, password, passwordConfirmation,
+      } = httpRequest.body;
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+      const isValid = this.emailValidation.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      const account = this.addAccount.add({
+        name,
+        email,
+        password,
+      });
+      // return {
+      //   statusCode: 201,
+      //   body: account,
+      // };
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
