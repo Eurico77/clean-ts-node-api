1664a9a6fb922960b2baca8c1213d1e06c053f1d
eurico77 <euricomagalhaes61@gmail.com>
2022-07-05 07:56:09 -0300

    feat: ensure create a new account with all values corrects and valids

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,26 +1,52 @@
+import { AddAccount } from '../../domain/usecases/add-account';
 import {
-  EmailValidator, Controller, HttpRequest, HttpResponse,
+  EmailValidator,
+  Controller,
+  HttpRequest,
+  HttpResponse,
 } from '../contracts';
 import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helper';
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidation: EmailValidator) {}
+  constructor(
+    private readonly emailValidation: EmailValidator,
+    private readonly addAccount: AddAccount,
+  ) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
-        const isValid = this.emailValidation.isValid(httpRequest.body.email);
-        if (!isValid) {
-          return badRequest(new InvalidParamError('email'));
-        }
       }
+      const {
+        name, email, password, passwordConfirmation,
+      } = httpRequest.body;
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+      const isValid = this.emailValidation.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      const account = this.addAccount.add({
+        name,
+        email,
+        password,
+      });
+      // return {
+      //   statusCode: 201,
+      //   body: account,
+      // };
     } catch (error) {
-      console.error(error);
       return serverError();
     }
   }
