1aa775afd00692c5eaec6a1ca6d2b02d1600b8e1
eurico77 <euricomagalhaes61@gmail.com>
2022-07-13 12:10:36 -0300

    refactor: adjustments patterns jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/**/index.ts'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
